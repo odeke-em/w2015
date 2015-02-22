add3551e735692a8793f326499dbe8cdeac2b212
Emmanuel Odeke <odeke@ualberta.ca>
2015-02-22 00:27:01 -0700

    initial code added

---
+++ client_soln_part2_base/build-cli/serial_handling.d
@@ -0,0 +1,13 @@
+build-cli/serial_handling.o: serial_handling.cpp serial_handling.h \
+  core_types.h \
+  /home/student/arduino-ua/arduino-1.0.1-32/hardware/arduino/cores/arduino/Arduino.h \
+  /home/student/arduino-ua/arduino-1.0.1-32/hardware/arduino/cores/arduino/binary.h \
+  /home/student/arduino-ua/arduino-1.0.1-32/hardware/arduino/cores/arduino/WCharacter.h \
+  /home/student/arduino-ua/arduino-1.0.1-32/hardware/arduino/cores/arduino/WString.h \
+  /home/student/arduino-ua/arduino-1.0.1-32/hardware/arduino/cores/arduino/HardwareSerial.h \
+  /home/student/arduino-ua/arduino-1.0.1-32/hardware/arduino/cores/arduino/Stream.h \
+  /home/student/arduino-ua/arduino-1.0.1-32/hardware/arduino/cores/arduino/Print.h \
+  /home/student/arduino-ua/arduino-1.0.1-32/hardware/arduino/cores/arduino/Printable.h \
+  /home/student/arduino-ua/arduino-1.0.1-32/hardware/arduino/cores/arduino/new.h \
+  /home/student/arduino-ua/arduino-1.0.1-32/hardware/arduino/variants/mega/pins_arduino.h \
+  /home/student/arduino-ua/arduino-1.0.1-32/libraries/UAUtils_Memory/assert13.h
